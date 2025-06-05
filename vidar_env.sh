66005120c4d6d2578f638bdd6c338331506fa099
Jim Brown <jpb@jimby.name>
2025-06-05 15:14:17 +0000

    Start moving to parameter based operation.

---
+++ vidar_env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# vidar_env.sh - vidar environment variables.
+#
+VIDAR_HOME=$HOME/src/vidar
+
+VIDAR_SEC=${VIDAR_HOME}/sec
+VIDAR_SCRIPTS=${VIDAR_HOME}/scripts
+
+# vidar output files
+VIDAR_LOGS=${VIDAR_HOME}/logs
+
+
