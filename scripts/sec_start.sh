e2a00e7bb515fa136050f478e790966820c221ff
Jim Brown <jpb@jimby.name>
2025-06-06 02:11:44 +0000

    Set up for automated start.

--- scripts/sec_start.sh
+++ scripts/sec_start.sh
@@ -24,13 +24,24 @@ done
 # rules files have been fixed up with fixup_rules.sh
 #
 
+# --detach : become a daemon
+# --tail   : keep reading input, even if changed
+# --log    : sec output goes here
+# --pid    : file holds sec process id
+# --conf   : a sec configuration file (multiples possible)
+# --input  : a sec input file (multiples possible)
+
+
 /usr/local/bin/sec  \
+  --detach \
+  --tail \
+  --log=${VIDAR_LOGS}/sec.output \
+  --pid=${VIDAR_SEC}/sec.pid \
   --conf=${VIDAR_SEC}/auth.rules \
   --conf=${VIDAR_SEC}/email.rules \
   --conf=${VIDAR_SEC}/nginx.rules \
   --input=${AUTHLOG} \
   --input=${EMAILLOG} \
-  --input=${NGINXLOG}
-
+  --input=${NGINXLOG} 
 
 
