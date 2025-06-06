180231c9710899e1662f0676d8a79c7c002fa75f
Jim Brown <jpb@jimby.name>
2025-06-06 16:23:14 +0000

    Add UDP port and refactor for one pass by sed.

--- sec/fixup_rules.sh
+++ sec/fixup_rules.sh
@@ -5,7 +5,7 @@
 #                   from  vidar_env.sh.
 #
 #  This script copies and  modifies *.rules.setup files
-#  by substituting all @@@LOGS@@@ tags with the value
+#  by substituting all @@@XXXXX@@@ tags with the value
 #  defined in vidar_env.sh.
 #
 #  The reason for this is that sec cannot apply substitutions
@@ -14,13 +14,18 @@
 # Pick up vidar environment for this run.
 . ../vidar_env.sh
 
-echo "Value LOGS=${VIDAR_LOGS} will be used..."
-
+# Fixup logs.
+echo "Value VIDAR_LOGS=${VIDAR_LOGS} will be used..."
 echo "s]@@@LOGS@@@]${VIDAR_LOGS}]" > f.sed
+
+# Fixup UDP port. UDP address of 127.0.0.1 is not affected.
+echo "Value VIDAR_UDP=${VIDAR_UDP} will be used..."
+echo "s]@@@PORT@@@]${VIDAR_UDP}]" >> f.sed
 
 cat auth.rules.setup     | sed  -f f.sed   > auth.rules
 cat email.rules.setup    | sed  -f f.sed   > email.rules
 cat nginx.rules.setup    | sed  -f f.sed   > nginx.rules
 cat calendar.rules.setup | sed  -f f.sed   > calendar.rules
 
-
+echo
+echo "Done."
