54d80a9a50e99947c43970c746e830a69bff5c3d
Jim Brown <jpb@jimby.name>
2025-06-07 22:29:28 +0000

    Refactor for shell pipe comms. Complete rework.

--- scripts/vidar_start.sh
+++ scripts/vidar_start.sh
@@ -2,23 +2,29 @@
 #
 # vidar_start.sh  - start vidar.
 #    Also load rules to allow ip, ipv6, icmp, icmp6 from any to any
+#    and runs necessary scripts.
 #    Must be root to run this script.
+
+echo
+echo "========  VIDAR: vidar_start.sh  ========="
+echo
 
 usage()
 {
   echo
-  echo "usage: vidar_start.sh  - start vidar. Do not load/unload ipfw.ko."
-  echo "       Also load rules 650** allow ip from any to any."
-  echo
+  echo "usage: vidar_start.sh  - starts Vidar."
+  echo "       Also loads rules '650** allow ip from any to any',"
+  echo "       starts the sec_start.sh script, and starts the"
+  echo "       vidar_add2BAD.sh script sending output to BAD.out"
+  echo "       in the configured logs directory."
   echo "  Must be root to run this script."
   echo "  Exiting..."
   echo
   exit 1
 }
 
-# Pick up environment for this run.
-. ../vidar_env.sh
-
+# Pick up environment for this run, and print it out.
+. ../vidar_env.sh 
 
 
 ME=`id -unr`
@@ -28,31 +34,70 @@ then
   usage;
 fi
 
-#set -x
+
+# Remove all logs first.  Safely.
+OLDLOGS=""
+
+echo
+echo "Removing old logs first..."
+
+
+if [ -z "${VIDAR_LOGS}" ]
+then # The VIDAR_LOGS variable is NOT SET. Bail out.
+  echo "The VIDAR_LOGS variable [${VIDAR_LOGS}] is NOT SET. Bailling out."
+  usage;
+else
+  for i in "${VIDAR_LOGS}"/*
+  do
+    if [ -f "${i}" ]
+    then
+#      echo "Adding ${i} to OLDLOGS"
+      OLDLOGS="${OLDLOGS} ${i}"
+    fi
+  done
+fi
+
+if [ ! -z "${OLDLOGS}" ]
+then 
+  echo "OLDLOGS=[${OLDLOGS}]"
+  echo
+  echo "USING rm -i ${OLDLOGS}"
+  echo
+  echo "Remove them all by 'y' and return, or not by hitting 'n' and return."
+  echo
+  rm -i ${OLDLOGS}
+else
+  echo "No logs to delete."
+fi
+
+
 
 # IPFW setup commands.  This script does NOT kldload ipfw.
 # Set up logging to /var/log/security and enable logging on rule 65000.
 # Use a here document to execute them in order.
 
-COMMANDS=$(cat <<EOF
-sysctl net.inet.ip.fw.verbose=1
-ipfw add 65000 allow log ip from any to any
-ipfw add 65005 allow log ipv6 from any to any
-ipfw add 65010 allow log icmp from any to any
-ipfw add 65015 allow log icmp6 from any to any
-ipfw add 65020 allow tcp from any to any established
-ipfw table BAD create type addr missing
-ipfw add 60000 deny ip from table\\\(BAD\\\) to any
-ipfw table BAD add 9999:9999:9999:9999:9999:9999:9999:9999
-ipfw table BAD list
-ipfw list
-EOF
-)
+#sysctl net.inet.ip.fw.verbose=1
 
-echo -n "Loading vidar... "
+# Create a file of ipfw commands.
+
+echo "kldunload ipfw"                                             >  /tmp/cmds.$$
+echo "kldload ipfw"                                               >> /tmp/cmds.$$
+echo "ipfw add 65000 allow ip from any to any"                    >> /tmp/cmds.$$
+echo "ipfw add 65005 allow ipv6 from any to any"                  >> /tmp/cmds.$$
+echo "ipfw add 65010 allow icmp from any to any"                  >> /tmp/cmds.$$
+echo "ipfw add 65015 allow icmp6 from any to any"                 >> /tmp/cmds.$$
+echo "ipfw add 65020 allow tcp from any to any established"       >> /tmp/cmds.$$
+echo "ipfw table BAD create type addr missing"                    >> /tmp/cmds.$$
+echo "ipfw add 60000 deny ip from table\\\\(BAD\\\\) to any"      >> /tmp/cmds.$$
+echo "ipfw table BAD add 9999:9999:9999:9999:9999:9999:9999:9999" >> /tmp/cmds.$$
+echo "ipfw table BAD list"                                        >> /tmp/cmds.$$
+echo "ipfw list"                                                  >> /tmp/cmds.$$
+
+echo -n "Loading Vidar... "
 
 
-echo "$COMMANDS" | while IFS= read CMD
+# Read and execute the commands one at a time (not in a subshell).
+while IFS= read CMD
 do
   echo "Running: [${CMD}]"
   sh -c "${CMD}"
@@ -63,11 +108,34 @@ do
     echo "Error on running command [${CMD}]. Return code [${RV}]"
     echo "  Unloading ipfw.  Check and try again."
     kldunload ipfw
+    rm -f /tmp/cmds.$$
     echo "  Exiting..."
     exit 1
   fi
   echo
-done
+done < /tmp/cmds.$$
 
+rm -f /tmp/cmds.$$
+
+
+# Start Sec.  The UDP port assignment and LOGS assignment are
+# set by the "fixup_rules.sh" script in the ${VIDAR_SEC} directory.
+# That script must be run every time you restart Vidar.  
+#
+
+#set -x
+#${VIDAR_SCRIPTS}/sec_start.sh  | ${VIDAR_SCRIPTS}/vidar_add2BAD.sh > ${VIDAR_LOGS}/add2BAD.out 2>&1 & 
+#${VIDAR_SCRIPTS}/sec_start.sh  
+
+
+cd ${VIDAR_SEC}
+
+# Fix rulesets.
+${VIDAR_SEC}/fixup_rules.sh
+
+# Let 'er rip...
+${VIDAR_SCRIPTS}/sec_start.sh 2>/dev/null  | ${VIDAR_SCRIPTS}/vidar_add2BAD.sh  
+
+# Script does not exit until killed.
 exit 0
 
