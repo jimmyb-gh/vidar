52db8f7b28de4da5470045c44899e0b742ca49c2
Jim Brown <jpb@jimby.name>
2025-06-07 22:10:15 +0000

    Refactor for shell pipe comms. Remove all PORT rules.

--- sec/fixup_rules.sh
+++ sec/fixup_rules.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 #
+
+echo 
+echo "========  VIDAR: fixup_rules.sh  ========="
+echo
+
+#
 #  fixup_rules.sh - fix *.rules.setup files to
 #                   substitute proper varialbles
 #                   from  vidar_env.sh.
@@ -11,7 +17,7 @@
 #  The reason for this is that sec cannot apply substitutions
 #  on rules files.  They are read "as is".
 
-# Pick up vidar environment for this run.
+# Pick up vidar environment for this run, and print it out.
 . ../vidar_env.sh
 
 # Fixup logs.
@@ -19,8 +25,8 @@ echo "Value VIDAR_LOGS=${VIDAR_LOGS} will be used..."
 echo "s]@@@LOGS@@@]${VIDAR_LOGS}]" > f.sed
 
 # Fixup UDP port. UDP address of 127.0.0.1 is not affected.
-echo "Value VIDAR_UDP=${VIDAR_UDP} will be used..."
-echo "s]@@@PORT@@@]${VIDAR_UDP}]" >> f.sed
+#echo "Value VIDAR_UDP=${VIDAR_UDP} will be used..."
+#echo "s]@@@PORT@@@]${VIDAR_UDP}]" >> f.sed
 
 cat auth.rules.setup     | sed  -f f.sed   > auth.rules
 cat email.rules.setup    | sed  -f f.sed   > email.rules
