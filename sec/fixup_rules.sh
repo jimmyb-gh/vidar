4f6211a7acbeffd251c20d4023f2b8e43ad15a73
Jim Brown <jpb@jimby.name>
2025-06-09 23:44:37 +0000

    Add SHOW_ENV code, delete all UDP port code, no longer used.

--- sec/fixup_rules.sh
+++ sec/fixup_rules.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 #
-
-echo 
-echo "========  VIDAR: fixup_rules.sh  ========="
-echo
-
 #
 #  fixup_rules.sh - fix *.rules.setup files to
 #                   substitute proper varialbles
@@ -17,21 +12,18 @@ echo
 #  The reason for this is that sec cannot apply substitutions
 #  on rules files.  They are read "as is".
 
-# Pick up vidar environment for this run, and print it out.
+# Pick up vidar environment for this run, and print it out unless "Q"uiet.
+
+SHOW_ENV="N"
 . ../vidar_env.sh
 
 # Fixup logs.
 echo "Value VIDAR_LOGS=${VIDAR_LOGS} will be used..."
 echo "s]@@@LOGS@@@]${VIDAR_LOGS}]" > f.sed
 
-# Fixup UDP port. UDP address of 127.0.0.1 is not affected.
-#echo "Value VIDAR_UDP=${VIDAR_UDP} will be used..."
-#echo "s]@@@PORT@@@]${VIDAR_UDP}]" >> f.sed
-
 cat auth.rules.setup     | sed  -f f.sed   > auth.rules
 cat email.rules.setup    | sed  -f f.sed   > email.rules
 cat nginx.rules.setup    | sed  -f f.sed   > nginx.rules
 cat calendar.rules.setup | sed  -f f.sed   > calendar.rules
 
-echo
-echo "Done."
+echo "Fixup done."
