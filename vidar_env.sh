8de54192501babf62255354911300bad806b3b91
Jim Brown <jpb@jimby.name>
2025-06-09 23:49:17 +0000

    Added VIDAR_UTILS, removed VIDAR_UDP, reworked output to use SHOW_ENV code.

--- vidar_env.sh
+++ vidar_env.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 #
-echo 
-echo "========  VIDAR: vidar_env.sh  ========="
-echo
-
 # vidar_env.sh - vidar environment variables.
 #
 # Base directory locations
 VIDAR_HOME=$HOME/src/vidar
 VIDAR_SEC=${VIDAR_HOME}/sec
 VIDAR_SCRIPTS=${VIDAR_HOME}/scripts
+VIDAR_UTILS=${VIDAR_HOME}/utils
 
 # Vidar output files
 #VIDAR_LOGS=/var/log/vidar
@@ -22,7 +19,6 @@ VIDAR_PIDS=${VIDAR_HOME}/pids
 # Normally these should be the actual files on the current system
 # but they can be overridden here.
 # These are INPUT logs, not OUTPUT logs.
-# They should eventually move to vidar_env.sh.
 #AUTHLOG=/var/log/auth.log
 #EMAILLOG=/var/log/maillog
 #NGINXLOG=/var/log/nginx/access.log
@@ -32,19 +28,16 @@ AUTHLOG=/root/src/input/auth.log
 EMAILLOG=/root/src/input/maillog
 NGINXLOG=/root/src/input/access.log
 
-# Vidar UDP port, used in Sec rules and in vidar_add2BAD.sh
-VIDAR_UDP=5555
-
 # Print the environment unless explicitly asked not to do so.
-
-if [ "$(echo "$1" | tr '[:lower:]' '[:upper:]')" != "Q" ]
+#
+if [ "X${SHOW_ENV}" = "XY" ]
 then
   echo "VIDAR Environment:"
   echo "VIDAR_HOME=    [${VIDAR_HOME}]"
   echo "VIDAR_SEC=     [${VIDAR_SEC}]"
   echo "VIDAR_SCRIPTS= [${VIDAR_SCRIPTS}]"
+  echo "VIDAR_UTILS=   [${VIDAR_UTILS}]"
   echo "VIDAR_PIDS=    [${VIDAR_PIDS}]"
-  echo "VIDAR_UDP=     [${VIDAR_UDP}]"
   echo "INPUTS:                                          OUTPUTS:"
   echo "AUTHLOG=  [${AUTHLOG}]            VIDAR_LOGS=[${VIDAR_LOGS}]"
   echo "EMAILLOG= [${EMAILLOG}]"
