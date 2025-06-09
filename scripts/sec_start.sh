c5e287ac6a9b1a0ed4436537fa28a43a0019ac31
Jim Brown <jpb@jimby.name>
2025-06-09 23:19:30 +0000

    Add SHOW_ENV, cleanup() function, and remove --detach option.

--- scripts/sec_start.sh
+++ scripts/sec_start.sh
@@ -4,16 +4,12 @@
 #                 configured by all rules in ./sec
 #                 and input files accordingly
 
-echo 
-echo "========  VIDAR: sec_start.sh  ========="
-echo
-
-
-
 # Pick up environment for this run, but don't print it out.
-. ../vidar_env.sh Q
 
-set -x
+SHOW_ENV="N"
+. ../vidar_env.sh
+
+# set -x
 
 for i in ${AUTHLOG} ${EMAILLOG} ${NGINXLOG}
 do
@@ -23,20 +19,35 @@ do
   fi
 done
 
+# Function to handle cleanup
+cleanup() {
+    echo "start_sec.sh Received signal, exiting..." >&2
+    echo "Killing sec via pid file."
+
+    if [ -f ${VIDAR_PIDS}/sec.pid ]
+    then
+        kill -TERM `cat ${VIDAR_PIDS}/sec.pid`
+        rm ${VIDAR_PIDS}/sec.pid
+    fi
+    exit 0
+}
+
+# Set up signal handlers
+trap cleanup INT TERM
 
 #
 # Put in a sanity check here to ensure that the
 # rules files have been fixed up with fixup_rules.sh
 #
 
-# --detach : become a daemon
 # --tail   : keep reading input, even if changed
 # --log    : sec output goes here
 # --pid    : file holds sec process id
 # --conf   : a sec configuration file (multiples possible)
 # --input  : a sec input file (multiples possible)
 
-#  --detach \
+# NOTE: Do not use --detach.  Sec is daemonized and this messes
+#       up the input stream for vidar_add2BAD.sh (which then exits).
 
 /usr/local/bin/sec  \
   --tail \
