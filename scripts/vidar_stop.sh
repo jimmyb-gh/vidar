d249e6e7e0189999d3995f118a17ffc3929c2775
Jim Brown <jpb@jimby.name>
2025-06-09 23:39:24 +0000

    - Added SHOW_ENV code.  - Reworked how to stop and unload IPFW.  - Reworked how to kill Vidar pipeline and show proof Sec is dead.

--- scripts/vidar_stop.sh
+++ scripts/vidar_stop.sh
@@ -6,7 +6,8 @@
 usage()
 {
   echo
-  echo "usage: vidar_stop.sh  - stop the firewall by unloading ipfw.ko"
+  echo "usage: vidar_stop.sh  - Stop the Vidar pipeline."
+  echo "                        Leave IPFW alone."
   echo
   echo "  Must be root to run this script."
   echo "  Exiting..."
@@ -21,25 +22,43 @@ then
   usage;
 fi
 
-# Pick up environment for this run, but don't print it out.
-. ../vidar_env.sh Q
+# Pick up environment for this run.
+
+SHOW_ENV="Y"
+. ../vidar_env.sh
 
 
-echo -n "Stopping and unloading ipfw... "
+#echo -n "Stopping and unloading ipfw... "
+#
+#STATUS=`/sbin/kldunload ipfw`
+#
+#if [ $? -ne 0 ]
+#then
+#  echo
+#  echo "Error on unloading ipfw. Return code [${STATUS}]"
+#  echo "  Check ipfw module, and try again."
+#  exit 2
+#else
+#  echo "done. ipfw kernel module unloaded."
+#  # show proof here.
+#  echo
+#fi
+#
 
-STATUS=`/sbin/kldunload ipfw`
+echo "Killing Vidar pipeline by killing Sec."
 
-if [ $? -ne 0 ]
+echo "kill -TERM \`cat ${VIDAR_PIDS}/sec.pid\`"
+kill -TERM `cat ${VIDAR_PIDS}/sec.pid`
+echo
+echo "Killing vidar_add2BAD.sh too, just for good measure."
+# Kill vidar_add2BAD.sh too, just in case it's throwing errors.
+kill -TERM `cat ${VIDAR_PIDS}/add2BAD.pid`
+
+if [ -f ${VIDAR_LOGS}/sec.output ]
 then
-  echo
-  echo "Error on unloading ipfw. Return code [${STATUS}]"
-  echo "  Check ipfw module, and try again."
-  exit 2
-else
-  echo "done. ipfw kernel module unloaded."
-  # show proof here.
-  echo
+    tail -3 ${VIDAR_LOGS}/sec.output
 fi
+
 
 exit 0
 
