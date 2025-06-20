a67d7a7cb4913daf2605527bde138c0a8d9c2916
Jim Brown <jpb@jimby.name>
2025-06-20 14:13:31 -0400

    vidar_dumpBAD.sh: Arrange to save the dump in archive dir and create a symlink to it through BAD.txt

--- scripts/vidar_dumpBAD.sh
+++ scripts/vidar_dumpBAD.sh
@@ -28,15 +28,25 @@ then
   usage;
 fi
 
+# Set up for this dump
+cd ${VIDAR_LOGS}
+
+# Make sure archive directory exists.
+mkdir -p ${VIDAR_LOGS}/archive
+
+export ARCHIVEDIR=${VIDAR_LOGS}/archive
+
+# ISO 8601 date spec
 DATESPEC=`date -j "+%Y%m%dT%H%M%S"`
 
 echo -n "Dumping BAD ... "
 
 COMMAND="/sbin/ipfw table BAD list"
 
-# echo "${COMMAND} \> ${VIDAR_LOGS}/BAD.${DATESPEC}"
+# echo "${COMMAND} \> ${ARCHIVEDIR}/BAD.${DATESPEC}"
 
-STATUS=`${COMMAND} > ${VIDAR_LOGS}/BAD.${DATESPEC}`
+STATUS=`${COMMAND} > ${ARCHIVEDIR}/BAD.${DATESPEC}`
+
 
 if [ $? -ne 0 ]
 then
@@ -46,10 +56,15 @@ then
   exit 2
 else
   echo "done. BAD table dumped in ${VIDAR_LOGS}."
-  ls -al ${VIDAR_LOGS}/BAD.${DATESPEC}
+  ls -al ${ARCHIVEDIR}/BAD.${DATESPEC}
+  # Remove existing link in logs file.
+  # Note that vidar_importBAD.sh uses this link.
+  rm ${VIDAR_LOGS}/BAD.txt
+  # And recreate it
+  ln -s ${ARCHIVEDIR}/BAD.${DATESPEC} ${VIDAR_LOGS}/BAD.txt
+  ls -al ${VIDAR_LOGS}
   echo
 fi
 
 exit 0
-
 
