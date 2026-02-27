fb5c0238e78114444b8849d6ab3311a562392afa
Jim Brown <jpb@jimby.name>
2026-02-27 16:41:21 -0500

    Add ipfw_queue table in preparation for variable block times with ipfw.

--- postgres/vidar.sql
+++ postgres/vidar.sql
@@ -23,6 +23,42 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: ipfw_queue; Type: TABLE; Schema: public; Owner: jpb
+--
+
+CREATE TABLE public.ipfw_queue (
+    id integer NOT NULL,
+    ip_addr inet NOT NULL,
+    added_at timestamp without time zone DEFAULT now() NOT NULL,
+    remove_after timestamp without time zone DEFAULT (now() + '24:00:00'::interval) NOT NULL
+);
+
+
+ALTER TABLE public.ipfw_queue OWNER TO jpb;
+
+--
+-- Name: ipfw_queue_id_seq; Type: SEQUENCE; Schema: public; Owner: jpb
+--
+
+CREATE SEQUENCE public.ipfw_queue_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.ipfw_queue_id_seq OWNER TO jpb;
+
+--
+-- Name: ipfw_queue_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: jpb
+--
+
+ALTER SEQUENCE public.ipfw_queue_id_seq OWNED BY public.ipfw_queue.id;
+
+
+--
 -- Name: offenders; Type: TABLE; Schema: public; Owner: jpb
 --
 
@@ -64,10 +100,25 @@ ALTER SEQUENCE public.offenders_id_seq OWNED BY public.offenders.id;
 
 
 --
+-- Name: ipfw_queue id; Type: DEFAULT; Schema: public; Owner: jpb
+--
+
+ALTER TABLE ONLY public.ipfw_queue ALTER COLUMN id SET DEFAULT nextval('public.ipfw_queue_id_seq'::regclass);
+
+
+--
 -- Name: offenders id; Type: DEFAULT; Schema: public; Owner: jpb
 --
 
 ALTER TABLE ONLY public.offenders ALTER COLUMN id SET DEFAULT nextval('public.offenders_id_seq'::regclass);
+
+
+--
+-- Name: ipfw_queue ipfw_queue_pkey; Type: CONSTRAINT; Schema: public; Owner: jpb
+--
+
+ALTER TABLE ONLY public.ipfw_queue
+    ADD CONSTRAINT ipfw_queue_pkey PRIMARY KEY (id);
 
 
 --
