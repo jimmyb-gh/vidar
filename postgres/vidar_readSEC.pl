0cb1a100954f92a87bb845e61640b3a11c7cc105
Jim Brown <jpb@jimby.name>
2026-02-27 17:07:47 -0500

    Changes to add ipfw 'blocktime' to ipfw_queue table.

--- postgres/vidar_readSEC.pl
+++ postgres/vidar_readSEC.pl
@@ -19,9 +19,19 @@ my $dbh = DBI->connect(
     { AutoCommit => 1, RaiseError => 1 }
 ) or die "Can't connect: $DBI::errstr";
 
+# Prepared statement for offenders table.
 my $sth = $dbh->prepare(
     "INSERT INTO offenders (offense_time, offender_ip, desc_line, entry, context, rule_num, evidence) 
      VALUES (?, ?::inet, ?, ?, ?, ?, ?)"
+);
+
+
+# Prepared statement for ipfw_queue table.
+# The blocking time comes through as an integer number of seconds to block (e.g. 3600 seconds).
+# The below syntax creates the correct removal time from that value.
+my $ipfwq = $dbh->prepare(
+    "INSERT INTO ipfw_queue (ip_addr, added_at, remove_after)
+     VALUES (?::inet, now(), now() + (? * interval '1 second'))"
 );
 
 
@@ -35,7 +45,9 @@ while (<STDIN>) {
 # DEBUGGING
     print STDERR "[$inputline]\n";
     
-    my ($time, $ip, $desc, $entry, $context, $rule, $evidence) = split /\|/, $_, 7;
+    # $blocktime is an integer number of seconds to block the IP.
+    # Each sec rule has it's own blocktime value or 0 (permanent block). See below.
+    my ($time, $ip, $desc, $entry, $context, $rule, $blocktime, $evidence) = split /\|/, $_, 8;
     
     # Sanitize inputs
     unless (defined $time && $time =~ /^\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}$/) {
@@ -62,15 +74,29 @@ while (<STDIN>) {
         $sth->execute($time, $ip, $desc, $entry, $context, $rule, $evidence);
     };
     if ($@) {
-        warn "Insert failed: $@";
+        warn "Insert into offenders failed: $@";
         next;
     }
-    
-    # Send validated IP to privileged updater
+
+    # Only execute this entry if the block is NOT zero.
+    # Zero (0) is a permanent block, so don't enter here, just pass the IP through.
+    if ($blocktime != 0) {
+      print STDERR "Blocking [$ip] for [$blocktime] on rule [$context, $rule]\n";
+      eval {
+          $ipfwq->execute($ip, $blocktime);
+      };
+      if ($@) {
+          warn "Insert into ipfw_queue failed: $@";
+          next;
+    }
+    else {
+      print STDERR "Blocking [$ip] permanently on rule [$context, $rule]\n";
+    }
+
+    # Send validated IP to add2BAD.pl script.
     print STDOUT "$ip\n";
 }
 
-# out jpbclose $ipfw_pipe;
 
 print STDERR "End of Program.  Closing DB connection\n";
 
