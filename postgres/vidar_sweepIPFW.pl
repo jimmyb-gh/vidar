75bd61c90ae125f7e0ee206e61b92f0191d418fc
Jim Brown <jpb@jimby.name>
2026-03-04 09:56:53 -0500

    Modify select statement to select low risk ip_addrs (blocktime < 600).  Also, add delete statement to remove ip_addrs from ipfw_queue.

--- postgres/vidar_sweepIPFW.pl
+++ postgres/vidar_sweepIPFW.pl
@@ -22,10 +22,17 @@ my $dbh = DBI->connect(
 
 #$dbh->{FetchHashKeyName} = 'NAME_lc';
 
-# Prepared statement for ipfw_queue table.
+# Prepared sweep statement for ipfw_queue table.
 # Look for entries that have aged out.
 my $sth = $dbh->prepare(
-    "select * from ipfw_queue where remove_after <  now() - interval '5 minutes' order by remove_after desc"
+    "select * from ipfw_queue where remove_after <  now() - interval '5 minutes' and blocktime <= 600 order by remove_after desc"
+);
+
+
+# Prepared delete statement for ipfw_queue table.
+# Look for entries that have aged out.
+my $delh = $dbh->prepare(
+    "delete from ipfw_queue where ip_addr = ? and blocktime <= 600"
 );
 
 
@@ -51,12 +58,37 @@ while ( $hash_ref = $sth->fetchrow_hashref ) {
 
 # DEBUGGING
 #    print STDERR "[@row]\n";
-    print STDERR "[ip_addr is $hash_ref->{'ip_addr'}\tblocktime is $hash_ref->{'blocktime'}]\n";
+#    print STDERR "[ip_addr is $hash_ref->{'ip_addr'}\tblocktime is $hash_ref->{'blocktime'}\tremove_after is $hash_ref->{'remove_after'}]\n";
+
+    $ip_addr = $hash_ref->{'ip_addr'};
 
 #    # Send validated IP to add2BAD.pl script.
 #    print STDOUT "$ip\n";
-}
 
+   # Safe execution - no shell interpolation
+    print STDERR "system('/sbin/ipfw', '-q', 'table', 'BAD', 'delete', $ip_addr);\n";
+
+
+    system('/sbin/ipfw', '-q', 'table', 'BAD', 'delete', $ip_addr);
+
+
+    # Delete from ipfw_queue
+
+    print STDERR "Delete $ip_addr from ipfw_queue\n";
+
+    # Execute
+    eval {
+        $delh->execute($ip_addr);
+    };
+
+    if ($@) {
+        warn "Delete statemetn failed!: $@";
+        exit;
+    }
+
+    sleep 1;
+
+}
 
 print STDERR "End of Program.  Closing DB connection\n";
 
