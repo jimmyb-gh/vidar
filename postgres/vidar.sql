4fa824e67f1604af99ecc3ffdcc17e17ee356ad3
Jim Brown <jpb@jimby.name>
2026-07-03 20:11:02 -0400

    Final cleanup on vidar.sql for better documentation, some additional field data checks, removed entry text field index.

--- postgres/vidar.sql
+++ postgres/vidar.sql
@@ -1,8 +1,7 @@
 
 \set ON_ERROR_STOP on
 
---- This is the master script for setting vidar roles, auths, and tables.
---- It combines _vidar_auth.sql and _vidar.sql into this one script.
+--- This is the master script for setting Vidar database roles, auths, and tables.
 ---
 --- After reinitializing postgresql this is the only script you need to run.
 ---
@@ -23,6 +22,9 @@
 --- Use this script from user postgres, not from user vidar!
 --- Example:
 ---   psql -U postgres -d postgres -f /usr/local/vidar/postgres/vidar.sql
+---
+--- NOTE: THIS SCRIPT DROPS THE Vidar DATABASE AND vidar ROLE IMMEDIATELY!!
+---       ALL DATA WILL BE REMOVED.  BACKUP YOUR DATABASE IF THIS IS A CONCERN.
 
 DROP DATABASE IF EXISTS vidar;
 DROP ROLE IF EXISTS vidar;
@@ -55,19 +57,21 @@ CREATE TABLE public.offenders (
     offender_ip       inet NOT NULL UNIQUE,
     desc_line         text NOT NULL,
     entry             text NOT NULL,
-    context           text NOT NULL CHECK (length(context) <= 20),
-    rule_num          integer NOT NULL,
+    context           text NOT NULL CHECK (length(context) <= 30),
+    rule_num          integer NOT NULL CHECK (rule_num > 0),
     permanent_block   integer NOT NULL DEFAULT 0 CHECK (permanent_block IN (0, 1)),
-    block_seconds     integer NOT NULL,
+    block_seconds     integer NOT NULL CHECK (block_seconds >= 0),
     active_block      integer NOT NULL DEFAULT 1 CHECK (active_block in (0,1)),
     remove_after      timestamp without time zone DEFAULT (now() + interval '24 hours') NOT NULL,
     ipfw_removed_at   timestamp without time zone DEFAULT NULL,
-    repeat_count      integer DEFAULT 1 NOT NULL,
+    repeat_count      integer NOT NULL DEFAULT 1 CHECK (repeat_count >= 1),
     evidence          text NOT NULL,
 
     CONSTRAINT offenders_active_removed_check
     CHECK (
-          NOT (active_block = 1 AND ipfw_removed_at IS NOT NULL)
+          (active_block = 1 AND ipfw_removed_at IS NULL)
+       OR
+          (active_block = 0 AND ipfw_removed_at IS NOT NULL)
     ) 
 );
 
@@ -78,7 +82,8 @@ CREATE INDEX idx_offenders_time ON public.offenders (offense_time);
 --- Not needed because of UNIQUE attribute on column
 --- CREATE INDEX idx_offenders_ip ON public.offenders (offender_ip);
 ---
-CREATE INDEX idx_offenders_entry ON public.offenders (entry);
+--- Not useful for a text field.  Do not index full raw log text.
+--- CREATE INDEX idx_offenders_entry ON public.offenders (entry);
 CREATE INDEX idx_offenders_context ON public.offenders (context);
 CREATE INDEX idx_offenders_rule ON public.offenders (rule_num);
 CREATE INDEX idx_offenders_permanent_block ON public.offenders (permanent_block);
@@ -110,16 +115,16 @@ INSERT INTO public.offenders (
     evidence
 )
 VALUES (
-    '2025-10-19 04:00:00',
+    '2025-01-01 01:00:00',
     '2001:db8:9999:9999:9999:9999:9999:9999',
-    'desc line',
-    'entry line',
-    'context value',
+    'dummy record desc line',
+    'dummy record entry line',
+    'dummy record context value',
     99999,
     1,
     0,
     1,
     'infinity',
     NULL,
-    'evidence text evidence text'
+    'dummy record evidence text inserted by vidar.sql'
 );
